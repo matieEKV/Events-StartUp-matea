0164a86ea3fe326ceecd2377495088bd4c246539
MKanunnikau <matea.ekv@gmail.com>
2026-04-28 23:33:22 +0200

    create Footer component and add styling

---
+++ app/app-vite/src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import styles from "./Footer.module.css";
+
+export const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <h3 className={styles.footerLogo}>EvenTeam</h3>
+    </footer>
+  );
+};
