2b83059afc20ace92e39b6f958490b3382b5c635
MKanunnikau <matea.ekv@gmail.com>
2026-05-06 11:10:11 +0200

    fetch the events from the database

--- app/app-vite/src/components/EventList/EventList.jsx
+++ app/app-vite/src/components/EventList/EventList.jsx
@@ -1,12 +1,30 @@
 import { useState } from "react";
 import { EventCard } from "../EventCard/EventCard.jsx";
 import styles from "./EventList.module.css";
+import { useFetchData } from "../FetchQueries/FetchQueries.jsx";
 
 // TODO: split each event below into its own EventCard component
 // TODO: add a "Buy ticket" button to each event card
 // TODO: replace the mock data import with a fetch call to GET /events
 
 export default function EventList({ events }) {
+  const { data, loading, error } = useFetchData();
+
+  const [isOnlyAvailable, setIsOnlyAvailable] = useState(false);
+  //TO BE ADDED LATER
+  // const [selectedCity, setSelectedCity] = useState("");
+  // const [sortOrder, setSortOrder] = useState("Date Ascending");
+
+  const processedEvents = data.filter((event) => {
+    if (isOnlyAvailable) {
+      return event.ticketsAvailable > 0;
+    }
+    return true;
+  });
+
+  function toggleEvents() {
+    setIsOnlyAvailable(!isOnlyAvailable);
+  }
   return (
     <ul className={styles.cardsContainer}>
       {events.map((event) => (
---
+++ app/app-vite/src/components/FetchQueries/FetchQueries.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import api from "../../api";
+
+export const useFetchData = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchData = async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      const targetUrl = api("/events");
+      console.log("Attempting to fetch from:", targetUrl);
+      const response = await fetch(api("events"));
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      setData(data);
+      console.log(data);
+    } catch (err) {
+      setError(err.message);
+      setLoading(false);
+      console.error("Fetch completely failed:", err);
+    }
+    setLoading(false);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return { data, loading, error };
+};
