f92e36ade1bc9e52b26694c5090fa8e8d59e5318
MKanunnikau <matea.ekv@gmail.com>
2026-05-07 21:23:38 +0200

    add pagination

--- app/app-vite/src/components/EventList/EventList.jsx
+++ app/app-vite/src/components/EventList/EventList.jsx
@@ -4,6 +4,7 @@ import styles from "./EventList.module.css";
 import { useFetchData } from "../../Hooks/FetchQueries.jsx";
 import { useFilterEvents } from "../../Hooks/FilterEvents.jsx";
 import { FilterBar } from "../FilterBar/FilterBar.jsx";
+import { Pagination } from "../Pagination/Pagination.jsx";
 
 // TODO: split each event below into its own EventCard component
 // TODO: add a "Buy ticket" button to each event card
@@ -15,11 +16,17 @@ export default function EventList({ events }) {
   const { isOnlyAvailable, toggleEvents, filteredEvents } =
     useFilterEvents(data);
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const [eventsPerPage, setEventsPerPage] = useState(5);
+
+  const lastEventIndex = currentPage * eventsPerPage;
+  const firstEventIndex = lastEventIndex - eventsPerPage;
+  const currentEvents = filteredEvents.slice(firstEventIndex, lastEventIndex);
   return (
     <>
       <FilterBar onChange={toggleEvents} />
       <ul className={styles.cardsContainer}>
-        {filteredEvents.map((event) => (
+        {currentEvents.map((event) => (
           <EventCard
             key={event.id}
             id={event.id}
@@ -37,6 +44,13 @@ export default function EventList({ events }) {
           ></EventCard>
         ))}
       </ul>
+      <div className={styles.pageButtonsContainer}>
+        <Pagination
+          totalEvents={filteredEvents.length}
+          eventsPerPage={eventsPerPage}
+          setCurrentPage={setCurrentPage}
+        />
+      </div>
     </>
   );
 }
