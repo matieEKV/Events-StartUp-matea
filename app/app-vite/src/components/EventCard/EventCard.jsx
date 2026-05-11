180d1ad5d1675cf042ab96500f68bb8b477fd1c0
MKanunnikau <matea.ekv@gmail.com>
2026-05-11 09:47:18 +0200

    refactor code as per reviewer's request - replace heading tags with p tags and styling - take computing logic outside of the component

--- app/app-vite/src/components/EventCard/EventCard.jsx
+++ app/app-vite/src/components/EventCard/EventCard.jsx
@@ -18,6 +18,9 @@ export const EventCard = ({
 }) => {
   const [isShowing, setIsShowing] = useState(false);
 
+  const ticketStatus =
+    ticketsAvailable === 0 ? "Sold out" : `${ticketsAvailable} tickets left`;
+
   function toggleSection() {
     setIsShowing(!isShowing);
   }
@@ -28,36 +31,48 @@ export const EventCard = ({
         <img className={styles.eventImage} src={image} alt="conference event" />
         <section className={styles.infoContainer}>
           <section className={styles.eventInfo}>
-            <h5 className={styles.label}>Title</h5>
-            <h4 className={styles.eventTitle}>{name}</h4>
-            <h5 className={styles.label}>Date and Time</h5>
+            <p className={styles.label}>
+              <strong>Title</strong>
+            </p>
+            <p className={styles.eventTitle}>{name}</p>
+            <p className={styles.label}>
+              <strong>Date | Time</strong>
+            </p>
             <div className={styles.dateTime}>
               <span>{date}</span>
               <span>{time}</span>
             </div>
 
-            <h5 className={styles.label}>Location</h5>
+            <p className={styles.label}>
+              <strong>Location</strong>
+            </p>
             <div className={styles.eventLocation}>
               <span>{venue}</span>
               <span>{city}</span>
             </div>
             {isShowing && (
               <>
-                <h5 className={styles.label}>Description</h5>
+                <p className={styles.label}>
+                  <strong>Description</strong>
+                </p>
                 <p className={styles.eventDescription}>{description}</p>
-                <h5 className={styles.label}>Price</h5>
+                <p className={styles.label}>
+                  <strong>Price</strong>
+                </p>
                 <p className={styles.price}>
                   {price === 0 ? "Free" : `€${price}`}
                 </p>
-                <h5 className={styles.label}>Tickets Available</h5>
-                <p className={styles.tickets}>
-                  {ticketsAvailable === 0
-                    ? "Sold out"
-                    : `${ticketsAvailable} tickets left`}
+                <p className={styles.label}>
+                  <strong>Tickets Available</strong>
                 </p>
-                <h5 className={styles.label}>Total Tickets</h5>
+                <p className={styles.tickets}>{ticketStatus}</p>
+                <p className={styles.label}>
+                  <strong>Total Tickets</strong>
+                </p>
                 <p className={styles.tickets}>{totalTickets}</p>
-                <h5 className={styles.label}>Category</h5>
+                <p className={styles.label}>
+                  <strong>Category</strong>
+                </p>
                 <p className={styles.category}>{category}</p>
               </>
             )}
