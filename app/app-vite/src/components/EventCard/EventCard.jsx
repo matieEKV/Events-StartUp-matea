57f700c74ad1092608958859476f3dedc1e214fc
MKanunnikau <matea.ekv@gmail.com>
2026-05-15 13:21:42 +0200

    changed styles of the cards to look nicer

--- app/app-vite/src/components/EventCard/EventCard.jsx
+++ app/app-vite/src/components/EventCard/EventCard.jsx
@@ -25,26 +25,65 @@ export const EventCard = ({ event }) => {
         />
         <section className={styles.infoContainer}>
           <section className={styles.eventInfo}>
-            <p className={styles.label}>
-              <strong>Title</strong>
-            </p>
-            <p className={styles.eventTitle}>{event.name}</p>
-            <p className={styles.label}>
-              <strong>Date | Time</strong>
+            <p className={styles.eventTitle}>
+              <strong>{event.name}</strong>
             </p>
             <div className={styles.dateTime}>
-              <span>{event.date}</span>
-              <span>{event.time}</span>
+              <p className={styles.dateTimeWithin}>
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <rect x="5" y="4" width="15" height="15" rx="2" ry="2"></rect>
+                  <line x1="16" y1="2" x2="16" y2="6"></line>
+                  <line x1="8" y1="2" x2="8" y2="6"></line>
+                  <line x1="5" y1="10" x2="18" y2="10"></line>
+                </svg>
+                <span>{event.date}</span>
+              </p>
+              <p className={styles.dateTimeWithin}>
+                <svg
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <circle cx="12" cy="12" r="10"></circle>
+                  <polyline points="12 6 12 12 16 14"></polyline>
+                </svg>
+                <span>{event.time}</span>
+              </p>
             </div>
-
-            <p className={styles.label}>
-              <strong>Location</strong>
-            </p>
             <div className={styles.eventLocation}>
-              <span>{event.venue}</span>
-              <span>{event.city}</span>
+              <svg
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
+                <circle cx="12" cy="10" r="3"></circle>
+              </svg>
+              <div className={styles.dtColumn}>
+                <span>{event.venue}</span>
+                <span>{event.city}</span>
+              </div>
             </div>
-            {isShowing && (
+            {/* {isShowing && (
               <>
                 <p className={styles.label}>
                   <strong>Description</strong>
@@ -67,13 +106,13 @@ export const EventCard = ({ event }) => {
                 </p>
                 <p className={styles.category}>{event.category}</p>
               </>
-            )}
+            )} */}
           </section>
         </section>
       </Link>
-      <button className={styles.toggleButton} onClick={toggleSection}>
+      {/* <button className={styles.toggleButton} onClick={toggleSection}>
         {isShowing ? "Show less" : "Show more"}
-      </button>
+      </button> */}
     </div>
   );
 };
