350b6ce87abd73dec97ed7b11118efd85153cd7a
MKanunnikau <matea.ekv@gmail.com>
2026-05-20 22:10:13 +0200

    update fetching hook to dynamically accept options for a header

--- app/app-vite/src/Hooks/FetchQueries.jsx
+++ app/app-vite/src/Hooks/FetchQueries.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import api from "../api";
 
-export const useFetchData = (url) => {
+export const useFetchData = (url, options = {}) => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -11,7 +11,7 @@ export const useFetchData = (url) => {
     setError(null);
 
     try {
-      const response = await fetch(api(url));
+      const response = await fetch(api(url), options);
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
